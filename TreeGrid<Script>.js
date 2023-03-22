ceed37aed9908ffe31ce9891bb51bf807f41c6b8
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2023-03-22 13:30:03 +0900

    Update TreeGrid<Script>.js

--- TreeGrid<Script>.js
+++ TreeGrid<Script>.js
@@ -8,4 +8,13 @@ Grids.OnValueChanged = function(grid,row, col, old_value, new_value){
 		}
 }
 
+row.kind  // 현재 렌더링 되는 행의 종류
+
+sgexpanded: 상위 노드가 펼쳐져서 하위 노드를 보여주는 상태인 행
+sgcollapsed: 상위 노드가 접혀서 하위 노드를 감추는 상태인 행
+sgleaf: 상위 노드를 가지지 않는 하위 노드인 행
+TreeGrid에서 row.kind 속성은 주로 사용자가 커스텀한 셀 스타일을 지정할 때 사용됩니다. 예를 들어, 
+sgexpanded 상태인 행은 다른 상태와 구분하기 위해 다른 색상을 지정할 수 있습니다. 또한,
+sgleaf 상태인 행은 아이콘을 변경하여 노드인지 아닌지를 시각적으로 나타낼 수 있습니다.
+
 oneDBIclick
