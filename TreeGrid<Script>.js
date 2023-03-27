4017be93c4ab082de17df04efcdb741a642ac0c3
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2023-03-27 14:46:55 +0900

    Update TreeGrid<Script>.js

--- TreeGrid<Script>.js
+++ TreeGrid<Script>.js
@@ -6,9 +6,15 @@ Grids.OnValueChanged = function(grid,row, col, old_value, new_value){
 			number = row.number;                                 // 해당 행의 ndx값을 가져온다. 
 		}else{                                                          // 체크박스 풀었을 경우
 		}
+}  //실제로 체크박스 체크라기보다는 해당 row에 변화가 있고 없고로 체크한다.
+
+// grid cfg 옵션에서 selecting 옵션이 1이고 체크를 한 경우 체크 값들 가져오는 방법
+var selected_rows = Grids["grid명"].GetSelRows();
+for (var i = 0; i<selected_rows.length; i++) {
+	console.log(selected_rows[i].cols이름+",");        
 }
 
-
+// 셀값 수정후 나오는 이벤트
 Grids.OnAfterSave = function(grid,source){
 	Grids.List.ReloadBody();
 }; 
@@ -18,11 +24,15 @@ Grids.OnAfterSave = function(grid,source){
 
 row.kind  // 현재 렌더링 되는 행의 종류
 
-sgexpanded: 상위 노드가 펼쳐져서 하위 노드를 보여주는 상태인 행
-sgcollapsed: 상위 노드가 접혀서 하위 노드를 감추는 상태인 행
-sgleaf: 상위 노드를 가지지 않는 하위 노드인 행
-TreeGrid에서 row.kind 속성은 주로 사용자가 커스텀한 셀 스타일을 지정할 때 사용됩니다. 예를 들어, 
-sgexpanded 상태인 행은 다른 상태와 구분하기 위해 다른 색상을 지정할 수 있습니다. 또한,
-sgleaf 상태인 행은 아이콘을 변경하여 노드인지 아닌지를 시각적으로 나타낼 수 있습니다.
+//그리드 row 더블클릭시 사용하는 함수
+Grids.OnDblClick = function(grid, row, col){}
+
+//그리드 row 한번 클릭시 이벤트
+Grids.OnClick = function(grid, row, col){}
+
+// 첨부파일 이미지 추가
+Grids.OnDataReceive = function(grid,source){
+	grid.SetAttribute(row, "열 이름", "열의 타입", "이미지 주소",1);	
+}
 
 var colIndex=myGrid.ColNames[1].indexOf("SELECT_YN");   // 트리그리드 열의 인덱스만 가져옴 객체는 아님
